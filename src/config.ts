2ccbcb66f5e31c3ffc0b3fe29661270757250e45
Kaushik Chemburkar <attherate@gmail.com>
2023-04-21 07:39:41 +1000

    about + 25 posts per page

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,7 @@ export const SITE: Site = {
   title: "kaushik.au",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
-  postPerPage: 3,
+  postPerPage: 25,
 };
 
 export const LOCALE = ["en-EN"]; // set to [] to use the environment default
